95e197fbe36eca44b33d96c0af2d02d4051de170
LelevV <45976878+LelevV@users.noreply.github.com>
2021-04-01 16:09:43 +0200

    added density LOB plot

--- dashboard/dash_stream.py
+++ dashboard/dash_stream.py
@@ -2,12 +2,12 @@ import dash
 from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import plotly
-import random
+from plotly.subplots import make_subplots
+
 import plotly.graph_objs as go
-from collections import deque
 import requests
+import numpy as np
+import pandas as pd
 
 
 BASE_ENDPOINT = "http://127.0.0.1:5000/"
@@ -16,12 +16,41 @@ ORDER_BOOK_ENDPOINT = BASE_ENDPOINT + "orderbook"
 
 def get_current_orderbook(endpoint):
     current_order_book = requests.get(endpoint).json()
-    bids = current_order_book['Bids']
-    asks = current_order_book['Asks']
+    return current_order_book
+
+
+def orderbook_to_scatter(orderbook_json):
+    bids = orderbook_json['Bids']
+    asks = orderbook_json['Asks']
     x = [o[0] for o in bids] + [o[0] for o in asks]
     y = [o[1] for o in bids] + [o[1] for o in asks]
-    c = ['Bid' for i in bids] + ['Ask' for i in asks]
+    c = ['Green' for i in bids] + ['Red' for i in asks]
     return x, y, c
+
+
+def orderbook_to_density(orderbook_json):
+    bids = (pd.DataFrame(orderbook_json['Bids'])
+            .groupby(0)
+            .sum()
+            .reset_index()
+            .sort_values(0, ascending=False)
+            )
+    bids_price = list(bids[0])
+    bids_qty = list(bids[1].cumsum())
+
+    asks = (pd.DataFrame(orderbook_json['Asks'])
+            .groupby(0)
+            .sum()
+            .reset_index()
+            .sort_values(0)
+            )
+    asks_price = list(asks[0])
+    asks_qty = list(asks[1].cumsum())
+
+    y = bids_qty + asks_qty
+    x = bids_price + asks_price
+
+    return x, y
 
 
 app = dash.Dash(__name__)
@@ -30,8 +59,7 @@ app.layout = html.Div(
         dcc.Graph(id='live-graph', animate=True),
         dcc.Interval(
             id='graph-update',
-            interval=1*1000
-        ),
+            interval=1*1000)
     ]
 )
 
@@ -39,15 +67,35 @@ app.layout = html.Div(
 @app.callback(Output('live-graph', 'figure'),
               Input('graph-update', 'n_intervals'))
 def update_graph_scatter(n):
-    X, Y, c = get_current_orderbook(ORDER_BOOK_ENDPOINT)
+
+    orderbook = get_current_orderbook(ORDER_BOOK_ENDPOINT)
+    x_scatter, y_scatter, c_scatter = orderbook_to_scatter(orderbook)
+    x_density, y_density = orderbook_to_density(orderbook)
+
     title = 'LOB'
-    fig = px.scatter(x=list(X),
-                     y=list(Y),
-                     color=c,
-                     title=title)
+
+    fig = make_subplots(rows=2,
+                        cols=1,
+                        subplot_titles=("LOB - Scatter", "LOB - Density")
+                        )
+
+    fig.append_trace(go.Scatter(x=x_scatter,
+                                y=y_scatter,
+                                marker={'color': c_scatter}
+                                ),
+                     row=1,
+                     col=1
+                     )
+    fig.append_trace(go.Scatter(x=list(x_density),
+                                y=list(y_density)),
+                     row=2,
+                     col=1
+                     )
+
     fig.update_traces(mode='markers', marker_line_width=2, marker_size=10)
     fig.update_xaxes(title_text='Price')
     fig.update_yaxes(title_text='Quantity')
+    fig.update_layout(height=700, showlegend=False)
     return fig
 
 
