2578316d8379bb25e74c407c84b0bc4329929099
LelevV <45976878+LelevV@users.noreply.github.com>
2021-04-07 19:00:38 +0200

    dashboard update (e.g. added vertical LOB)

--- dashboard/dash_stream.py
+++ dashboard/dash_stream.py
@@ -26,8 +26,30 @@ def get_trades(endpoint):
 
 def json_to_trades(trades_json):
     cols = ['TradeID', 'TradeTime', 'Price', 'Qty', 'BuyerMaker']
-    trades = pd.DataFrame(trades_json['Trades'], columns=cols)
+    trades = (pd.DataFrame(trades_json['Trades'], columns=cols)
+              .assign(TradeTime=lambda x: pd.to_datetime(x['TradeTime']))
+              )
     return trades
+
+
+def orderbook_to_vertical(orderbook_json):
+    # give bids negative rank for plot
+    bids = (pd.DataFrame(orderbook_json['Bids'], columns=['Price', 'Quantity'])
+            .assign(Quantity=lambda x: x['Quantity'],
+                    Color='Green',
+                    id=lambda x: x.index,
+                    Rank=lambda x: -x.groupby('Price')['id'].rank('first')))
+
+    asks = (pd.DataFrame(orderbook_json['Asks'], columns=['Price', 'Quantity'])
+            .assign(Color='Red',
+                    id=lambda x: x.index,
+                    Rank=lambda x: x.groupby('Price')['id'].rank('first')))
+    df = pd.concat([bids, asks]).sort_values('Price')
+    x = list(df['Rank'])
+    y = list(df['Price'])
+    c = list(df['Color'])
+    q = list(df['Quantity'])
+    return x, y, c, q
 
 
 def orderbook_to_scatter(orderbook_json):
@@ -44,8 +66,7 @@ def orderbook_to_density(orderbook_json):
             .groupby(0)
             .sum()
             .reset_index()
-            .sort_values(0, ascending=False)
-            )
+            .sort_values(0, ascending=False))
     bids_price = list(bids[0])
     bids_qty = list(bids[1].cumsum())
 
@@ -53,8 +74,7 @@ def orderbook_to_density(orderbook_json):
             .groupby(0)
             .sum()
             .reset_index()
-            .sort_values(0)
-            )
+            .sort_values(0))
     asks_price = list(asks[0])
     asks_qty = list(asks[1].cumsum())
 
@@ -83,38 +103,67 @@ def update_graph_scatter(n):
     orderbook = get_current_orderbook(ORDER_BOOK_ENDPOINT)
     x_scatter, y_scatter, c_scatter = orderbook_to_scatter(orderbook)
     x_density, y_density, c_density = orderbook_to_density(orderbook)
+    x_vertical_lob, y_vertical_lob, c_vertical_lob, quantity_vertical_lob = orderbook_to_vertical(orderbook)
 
     trades_json = get_trades(TRADES_ENDPOINT)
     trades = json_to_trades(trades_json)
-    print(trades)
 
+    x_price = list(trades['TradeTime'])
+    y_price = list(trades['Price'])
+    # sort descending for trades display
+    trades = trades.sort_values('TradeTime', ascending=False)
 
-    fig = make_subplots(rows=3,
-                        cols=1,
-                        subplot_titles=("LOB - Scatter", "LOB - Density", 'Trades'),
-                        specs=[[{"type": "scatter"}],
-                               [{"type": "scatter"}],
-                               [{"type": "table"}]]
+    fig = make_subplots(rows=2,
+                        cols=2,
+                        subplot_titles=("Price", "CLOB", "CLOB - Density", 'Trades'),
+                        # row_heights=[0.15, 0.55, 0.15, 0.15],
+                        specs=[[{"type": "scatter"}, {"type": "scatter"}],
+                               [{"type": "scatter"}, {"type": "table"}]]
                         )
 
-    fig.append_trace(go.Scatter(x=x_scatter,
-                                y=y_scatter,
-                                marker={'color': c_scatter}
+    # add price chart
+    row_1 = 1
+    col_1 = 1
+    fig.append_trace(go.Scatter(x=x_price,
+                                y=y_price,
+                                mode='lines+markers'
                                 ),
-                     row=1,
-                     col=1
-                     )
+                     row=row_1,
+                     col=col_1)
+    fig.update_xaxes(row=row_1, col=col_1, range=[x_price[0]-pd.Timedelta(seconds=3), x_price[-1]+pd.Timedelta(seconds=1)])
+
+    # add clob
+    row_2 = 1
+    col_2 = 2
+    fig.append_trace(go.Scatter(y=y_vertical_lob,
+                                x=x_vertical_lob,
+                                text=quantity_vertical_lob,
+                                # orientation='h',
+                                marker={'color': c_vertical_lob},
+                                mode='markers+text',
+                                textposition="top center"),
+                     row=row_2,
+                     col=col_2)
+    # fig.update_traces(row=2, col=1, mode='markers', marker_line_width=1, marker_size=10, marker_line_color='white')
+    fig.update_xaxes(row=row_2, col=col_2, title_text='Quantity')
+    fig.update_yaxes(row=row_2, col=col_2, title_text='Price')
+
+    # add density orderbook
+    row_3 = 2
+    col_3 = 1
     fig.append_trace(go.Scatter(x=list(x_density),
                                 y=list(y_density),
-                                marker={'color': c_density}),
-                     row=2,
-                     col=1
-                     )
+                                marker={'color': c_density},
+                                ),
+                     row=row_3,
+                     col=1)
+    fig.update_traces(row=row_3, col=col_3, mode='markers', marker_line_width=1, marker_size=10, marker_line_color='white')
+    fig.update_xaxes(row=row_3, col=col_3, title_text='Price')
+    fig.update_yaxes(row=row_3, col=col_3, title_text='Quantity')
 
-    fig.update_traces(mode='markers', marker_line_width=2, marker_size=10)
-    fig.update_xaxes(title_text='Price')
-    fig.update_yaxes(title_text='Quantity')
-
+    # add transactions
+    row_4 = 2
+    col_4 = 2
     fig.append_trace(go.Table(header=dict(values=trades.columns),
                               cells=dict(values=[list(trades[trades.columns[0]]),
                                                  list(trades[trades.columns[1]]),
@@ -122,11 +171,12 @@ def update_graph_scatter(n):
                                                  list(trades[trades.columns[3]]),
                                                  list(trades[trades.columns[4]])
                                                  ])),
-                     row=3,
-                     col=1
-                     )
+                     row=row_4,
+                     col=col_4)
 
-    fig.update_layout(height=1400, showlegend=False)
+    fig.update_layout(height=1200, showlegend=False,
+                      template='plotly_dark'
+                      )
     return fig
 
 
--- multithreading_test.py
+++ multithreading_test.py
@@ -10,15 +10,15 @@ def run_agent(agent_id, time_secs, update_interval):
 n_agents = 9
 pool = ThreadPool(processes=n_agents)
 # a sequence of argument tuples for starmap function
-parameter_settings = [('A', 100, 5),
-                      ('B', 100, 3),
-                      ('C', 100, 2),
-                      ('D', 100, 5),
-                      ('E', 100, 3),
-                      ('F', 100, 2),
-                      ('G', 100, 5),
-                      ('H', 100, 3),
-                      ('I', 100, 3)
+parameter_settings = [('A', 1000, 1),
+                      ('B', 1000, 1),
+                      ('C', 1000, 1),
+                      ('D', 1000, 1),
+                      ('E', 1000, 1),
+                      ('F', 1000, 1),
+                      ('G', 1000, 1),
+                      ('H', 1000, 1),
+                      ('I', 1000, 0.5)
                       ]
 assert len(parameter_settings) == n_agents
 results = pool.starmap(run_agent, parameter_settings)
